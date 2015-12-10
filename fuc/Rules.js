cd7857c35249706926669bfcfc3d1f6f2c92dd83
苏伟明 <byte_su@163.com>
2015-12-10 22:58:23 +0800

    xbiao

--- fuc/Rules.js
+++ fuc/Rules.js
@@ -17,13 +17,29 @@ module.exports = [
             },
             current:function($,num){
                 var url  = $('.pagination li').last().find('a').attr('href');
-                console.log('AAAA')
+                // console.log('AAAA')
                 console.log((url.slice(0,url.lastIndexOf('=')+1-url.length)+num))
                 return (url.slice(0,url.lastIndexOf('=')+1-url.length)+num);
             }
         },
         item:{
             url:'#topic_list .cell'
+        }
+    },
+    {   
+        site: 'http://watch.xbiao.com/',
+        list_item:'#list-pic > ul >li',
+        next_page:function(page){ // page 列表页
+            return 'http://watch.xbiao.com/p'+page+'.html';
+        },
+        match_title:function($,ele){//标题
+            return $(ele).find('div.link > a').text();
+        },
+        match_link:function($,ele){ //链接
+            return $(ele).find('div.link > a').attr('href');
+        },
+        current_page:function($){
+            return $('.pages').find('.act').html();
         }
     },
 ];
--- index.cnodejs.js
+++ index.cnodejs.js
@@ -22,19 +22,32 @@ function crawler() {
         //    parseSite(site);
         request(site.site, function (error, res, body) {
             if (!error && res.statusCode == 200) {
+                console.log('AAAA')
+                //console.log(site.page.total($));
                 var $ = cheerio.load(body);
                 var current = $('.pagination').attr('current_page');
+                //console.log($.html())
+
                 if (!isNaN(current)) {
                     console.log(current)
                 }
+                
                 console.log(current);
+                console.log('AAA');
+
+                //var i = 1;
+                //while (i <= site.page.total($)) {
+                //    //parsePage($, site);
+                //    console.log(path.join(site.site,site.page.current($,i)));
+                //    i++;
+                //}
             }
         })
     });
 }
-console.time('time->');
-var items = rules[1];
-parseSite(items.site).then(function(res){
+// console.time('time->');
+
+parseSite('http://cnodejs.org/').then(function(res){
     console.log(res.length);
     console.timeEnd('time->');
 }).catch(function(e){
@@ -45,16 +58,18 @@ function parseSite(url,prevRes) {
     request(url, function (error, res, body) {
         if (!error && res.statusCode == 200) {
             var $ = cheerio.load(body);
-            var current = items.current_page($);
-            console.log(current);
-            
-            if (!isNaN(current)&&current<10) { // ""为true
+            var current = $('.pagination').attr('current_page');
+            console.log(current)
+            //if(current){
+            //    defer.resolve([]);
+            //}else
+            if (!isNaN(current)&&current) { // ""为true
                 current = parseInt(current) + 1 ;
-                Q.all([parseSite(items.next_page(current)), parsePage($, items.list_item)]).then(function(allRes){
+                Q.all([parseSite('https://cnodejs.org/?tab=all&page='+current), parsePage($, rules[0])]).then(function(allRes){
                     defer.resolve(allRes[0].concat(allRes[1]));
                 });
             }else{
-                parsePage($, items.list_item).then(defer.resolve).catch(defer.reject);
+                parsePage($, rules[0]).then(defer.resolve).catch(defer.reject);
             }
         } else {
             defer.reject(error);
@@ -68,25 +83,28 @@ function parsePage($, site) {
     var defer = Q.defer();
     try{
         var arr = [];
-        var li = $(site);
-        console.log($(site).length)
+        var li = $(site.item.url);
         li.each(function (index, ele) {
-            
-            var text = items.match_title($,ele).trim();
-            var href = items.match_link($,ele);
-            
+            //console.log(($(ele).html()))
+            var text = $(ele).find('.topic_title').text().trim();
+            var href = $(ele).find('.topic_title').attr('href');
+            //console.log(site.site, href)
             var obj = {
-                href: path.join('', href),
+                href: path.join(site.site, href),
                 text: text,
-                md5: tool.md5Str(path.join('', href))
+                md5: tool.md5Str(path.join(site.site, href))
             };
             test[tool.md5Str(obj.href)] = obj;
-            console.log(JSON.stringify(obj));
+            //console.log(JSON.stringify(obj));
+            console.log(text)
             arr.push(obj);
         });
         defer.resolve(arr);
     }catch(e){
         defer.reject(e);
     }
+
+    //console.log(Object.keys(test).length)
+    //console.log("arr:", arr.length);
     return defer.promise;
 }
